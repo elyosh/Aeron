c88f8792883b07ef55a3cef16efaeca107e6fa55
Adrien Moulin <adrien@rgb-labs.io>
2026-07-28 11:46:50 +0200

    Add SDL application entry point integration

---
+++ include/aeron/main.h
@@ -0,0 +1,10 @@
+#ifndef AERON_MAIN_H
+#define AERON_MAIN_H
+
+/*
+ * Include this header exactly once, from the translation unit that defines
+ * main(). SDL supplies the platform entry point and forwards it to main().
+ */
+#include <SDL3/SDL_main.h>
+
+#endif
