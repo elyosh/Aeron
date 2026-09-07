88e4cd07a5f0e4c5a5307a5b5b43f59d0170f33a
Adrien Moulin <adrien@rgb-labs.io>
2026-09-07 22:26:13 +0200

    Format DirectPlay ICE declarations

--- src/compat/dplay_ice.h
+++ src/compat/dplay_ice.h
@@ -8,13 +8,13 @@ typedef uint64_t DpLink;
 void             DpIce_Update(void);
 int              DpIce_Receive(DpLink* link, void* data, unsigned* size);
 /* 1: sent, 0: would block/not connected, -1: failed. */
-int              DpIce_Send(DpLink link, const void* data, unsigned size);
-int              DpIce_Matches(DpLink link, const GUID* application, const GUID* room);
-DpLink           DpIce_Prepared(const GUID* application, const GUID* room);
-void             DpIce_Claim(DpLink link);
-void             DpIce_Admit(DpLink link);
-void             DpIce_Retire(DpLink link);
-void             DpIce_CancelJoin(uint64_t generation);
-int              DpIce_Active(void);
-void             DpIce_Reset(void);
+int    DpIce_Send(DpLink link, const void* data, unsigned size);
+int    DpIce_Matches(DpLink link, const GUID* application, const GUID* room);
+DpLink DpIce_Prepared(const GUID* application, const GUID* room);
+void   DpIce_Claim(DpLink link);
+void   DpIce_Admit(DpLink link);
+void   DpIce_Retire(DpLink link);
+void   DpIce_CancelJoin(uint64_t generation);
+int    DpIce_Active(void);
+void   DpIce_Reset(void);
 #endif
